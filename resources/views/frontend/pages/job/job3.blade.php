282316febdbf8b0a2b7257e5712295080c96c8a6
Roshan Dhungana <dhunganaroshan341@gmail.com>
2026-04-15 10:06:14 +0545

    fixed:custom - button "

--- resources/views/frontend/pages/job/job3.blade.php
+++ resources/views/frontend/pages/job/job3.blade.php
@@ -36,8 +36,10 @@ $css =
     }
 
     .custom-nice-select {
-        width: 200px !important;
-        padding: 4px 0.5em !important;
+
+        /* width: 35% !important; */
+        padding: 0px 0.5em !important;
+
     }
 
     .nice-select:before {
