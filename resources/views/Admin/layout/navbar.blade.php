2a98c3c773163b14f22efbac03e0d9224cf8ea9e
Roshan Dhungana <dhunganaroshan341@gmail.com>
2026-04-19 09:29:49 +0545

    feat:added applications in admin,fix:some frontend errors

--- resources/views/Admin/layout/navbar.blade.php
+++ resources/views/Admin/layout/navbar.blade.php
@@ -44,7 +44,8 @@
 
                 <!-- <li class="nav-item"><a class="nav-link" href="#"><i class="mdi mdi-calendar-check"></i>
                         Job Seekers</a></li> -->
-                <li class="nav-item"><a class="nav-link" href="#"><i class="mdi mdi-calendar-check"></i>
+                <li class="nav-item"><a class="nav-link" href="{{ route('admin.applicatiions.index') }}"><i
+                            class="mdi mdi-calendar-check"></i>
                         Applications</a></li>
             </ul>
         </div>
